fa4f8c4251abda6a58d38eaf8e08943e0ecbf322
zenin5479 <zenin5479@gmail.com>
2026-04-07 15:04:49 +0300

    Update 1.1 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,40 @@ namespace ConsoleApp
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         var process = new Process
+         {
+            StartInfo = new ProcessStartInfo
+            {
+               FileName = "WinFormsApp.exe",
+               UseShellExecute = false,
+               RedirectStandardInput = true,
+               RedirectStandardOutput = true
+            }
+         };
+         process.Start();
+
+         string input;
+         do
+         {
+            Console.Write("Введите сообщение (или 'exit'): ");
+            input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) continue;
+
+            // Отправляем запрос
+            process.StandardInput.WriteLine(input);
+            process.StandardInput.Flush();
+
+            if (input.ToLower() == "exit")
+               break;
+
+            // Ждём ответ (блокирует консоль)
+            string response = process.StandardOutput.ReadLine();
+            Console.WriteLine($"Ответ WinForms: {response}");
+
+         } while (true);
+
+         process.WaitForExit(1000);
+         if (!process.HasExited) process.Kill();
       }
    }
 }
