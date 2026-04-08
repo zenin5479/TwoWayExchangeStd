187392a338e8e79950b336c59d0e806fc68f0599
zenin5479 <zenin5479@gmail.com>
2026-04-08 10:08:32 +0300

    Update 2.9 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.cs
+++ WinFormsApp/MainForm.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace WinFormsApp
 {
@@ -8,66 +7,6 @@ namespace WinFormsApp
       public MainForm()
       {
          InitializeComponent();
-
-         // Запускаем цикл обмена после того, как форма показана
-         Shown += MainForm_Shown;
-      }
-
-      private void MainForm_Shown(object sender, EventArgs e)
-      {
-         // Основной цикл обмена – блокирует UI поток, но это разрешено
-         while (true)
-         {
-            // 1. Синхронно читаем запрос из stdin (блокирует форму)
-            string request = Console.ReadLine();
-            if (request == null)
-            {
-               // Консоль закрыла поток
-               break;
-            }
-
-            if (request.ToLower() == "exit")
-            {
-               break;
-            }
-
-            // 2. Показываем запрос в форме (форма всё ещё не реагирует на клики, но текст обновится)
-            lblStatus.Text = string.Format("Получен запрос: {0}", request);
-            txtLastRequest.Text = request;
-            // Принудительная перерисовка
-            Refresh();
-
-            // 3. Показываем модальное диалоговое окно для ввода ответа оно блокирует выполнение кода
-            string response = ShowInputDialog(request);
-            if (response == null)
-            {
-               response = "";
-            }
-
-            // 4. Отправляем ответ обратно в консоль
-            Console.WriteLine(response);
-            lblStatus.Text = "Ожидание следующего запроса...";
-         }
-
-         // По завершении цикла закрываем приложение
-         Application.Exit();
-      }
-
-      private string ShowInputDialog(string request)
-      {
-         // Простой модальный диалог через форму
-         using (MainForm dialog = new MainForm())
-         {
-            Label label = new Label { Text = request, Location = new System.Drawing.Point(10, 10), AutoSize = true };
-            dialog.Controls.Add(label);
-
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-               return txtLastRequest.Text;
-            }
-
-            return null;
-         }
       }
    }
 }
