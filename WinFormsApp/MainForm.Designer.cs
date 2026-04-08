a6b6139ed52afd274a7010777541d4250525f600
zenin5479 <zenin5479@gmail.com>
2026-04-08 21:29:07 +0300

    Update 5.4 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.Designer.cs
+++ WinFormsApp/MainForm.Designer.cs
@@ -29,20 +29,6 @@ namespace WinFormsApp
       ///  Required method for Designer support - do not modify
       ///  the contents of this method with the code editor.
       /// </summary>
-      private void InitializeComponent()
-      {
-         SuspendLayout();
-         // 
-         // MainForm
-         // 
-         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(389, 173);
-         Name = "MainForm";
-         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-         Text = "Двусторонний обмен WinForms и консоли";
-         ResumeLayout(false);
-      }
 
       #endregion
    }
