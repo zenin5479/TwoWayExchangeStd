62606d8e847c5ed40351359be2c26cf432b4c11d
zenin5479 <zenin5479@gmail.com>
2026-04-08 21:10:28 +0300

    Update 4.6 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/Program.cs
+++ WinFormsApp/Program.cs
@@ -11,9 +11,17 @@ namespace WinFormsApp
       [STAThread]
       static void Main()
       {
-         Application.EnableVisualStyles();
-         Application.SetCompatibleTextRenderingDefault(false);
-         Application.Run(new MainForm());
+         try
+         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show($"Критическая ошибка приложения: {ex.Message}",
+               "Ошибка запуска", MessageBoxButtons.OK, MessageBoxIcon.Error);
+         }
       }
    }
 }
