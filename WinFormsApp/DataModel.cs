707bcc24e8d1f703298b172aa736357c6a0e73a7
zenin5479 <zenin5479@gmail.com>
2026-04-08 10:06:26 +0300

    Update 2.7 Обновлен метод. Проведен рефакторинг

---
+++ WinFormsApp/DataModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WinFormsApp
+{
+   public class DataModel
+   {
+      private string _consoleMessage;
+      private string _formMessage;
+
+      public string ConsoleMessage
+      {
+         get => _consoleMessage;
+         set
+         {
+            _consoleMessage = value;
+            OnMessageChanged?.Invoke(this, new MessageEventArgs("Console", value));
+         }
+      }
+
+      public string FormMessage
+      {
+         get => _formMessage;
+         set
+         {
+            _formMessage = value;
+            OnMessageChanged?.Invoke(this, new MessageEventArgs("Form", value));
+         }
+      }
+
+      public event EventHandler<MessageEventArgs> OnMessageChanged;
+   }
+
+   public class MessageEventArgs : EventArgs
+   {
+      public string Source { get; }
+      public string Message { get; }
+
+      public MessageEventArgs(string source, string message)
+      {
+         Source = source;
+         Message = message;
+      }
+   }
+}
