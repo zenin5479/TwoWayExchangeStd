2437f941d0e8eed14aba1f845ba360cb50d8ffcc
zenin5479 <zenin5479@gmail.com>
2026-04-09 17:47:43 +0300

    Update 10.6 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/MyForm.cs
+++ ConsoleApp/MyForm.cs
@@ -1,8 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ConsoleApp
 {
-
    // Определение простой формы
    public class MyForm : Form
    {
@@ -11,18 +12,23 @@ namespace ConsoleApp
       public MyForm()
       {
          // Настройка формы
-         this.Text = "Моя форма из консоли";
-         this.Width = 300;
-         this.Height = 200;
+         Text = "Моя форма из консоли";
+         Width = 300;
+         Height = 200;
 
          // Создание кнопки
          _closeButton = new Button();
          _closeButton.Text = "Закрыть";
-         _closeButton.Location = new System.Drawing.Point(100, 70);
-         _closeButton.Click += (sender, e) => { this.Close(); };
+         _closeButton.Location = new Point(100, 70);
+         _closeButton.Click += OnCloseButtonOnClick;
 
          // Добавление кнопки на форму
-         this.Controls.Add(_closeButton);
+         Controls.Add(_closeButton);
+      }
+
+      private void OnCloseButtonOnClick(object sender, EventArgs e)
+      {
+         Close();
       }
    }
 }
