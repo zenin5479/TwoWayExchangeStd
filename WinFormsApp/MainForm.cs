b7ea727fb2de56656c8a71203e4e45249b52d2ce
zenin5479 <zenin5479@gmail.com>
2026-04-09 09:56:02 +0300

    Update 6.3 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.cs
+++ WinFormsApp/MainForm.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows.Forms;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
 namespace WinFormsApp
 {
    public partial class MainForm : Form
    {
+      private Process childProcess;
+      private StreamWriter childInput;
+      private StreamReader childOutput;
+
       public MainForm()
       {
          InitializeComponent();
