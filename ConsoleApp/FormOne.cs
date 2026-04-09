302940c793c43985e381fbd74edf0f9d9f8c155b
zenin5479 <zenin5479@gmail.com>
2026-04-09 18:37:32 +0300

    Update 11.9 Обновлен метод. Проведен рефакторинг. Рабочий код

---
+++ ConsoleApp/FormOne.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ConsoleApp
+{
+   public partial class FormOne : Form
+   {
+      public FormOne()
+      {
+         InitializeComponent();
+      }
+   }
+}
