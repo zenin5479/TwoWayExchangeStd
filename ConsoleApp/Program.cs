d0aad80030b5cb0117d4abb075bcbd658a79f39f
zenin5479 <zenin5479@gmail.com>
2026-04-09 17:45:27 +0300

    Update 10.5 Обновлен метод. Проведен рефакторинг

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -28,25 +28,4 @@ namespace ConsoleApp
    }
 
    // Определение простой формы
-   public class MyForm : Form
-   {
-      private Button _closeButton;
-
-      public MyForm()
-      {
-         // Настройка формы
-         this.Text = "Моя форма из консоли";
-         this.Width = 300;
-         this.Height = 200;
-
-         // Создание кнопки
-         _closeButton = new Button();
-         _closeButton.Text = "Закрыть";
-         _closeButton.Location = new System.Drawing.Point(100, 70);
-         _closeButton.Click += (sender, e) => { this.Close(); };
-
-         // Добавление кнопки на форму
-         this.Controls.Add(_closeButton);
-      }
-   }
 }
