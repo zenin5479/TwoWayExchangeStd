083a07c8cb7ecedaf154a5e2405fc4565a180525
zenin5479 <zenin5479@gmail.com>
2026-04-09 18:17:12 +0300

    Update 11.5 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleApp/DataForm.cs
+++ ConsoleApp/DataForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ConsoleApp
@@ -58,13 +59,15 @@ namespace ConsoleApp
             Width = 200
          };
 
-         _closeButton.Click += (sender, e) =>
+         void OnCloseButtonOnClick(object sender, EventArgs e)
          {
             // Перед закрытием сохраняем данные в свойства
             UserAgreed = agreeCheckBox.Checked;
             UserComment = commentTextBox.Text;
             Close();
-         };
+         }
+
+         _closeButton.Click += OnCloseButtonOnClick;
 
          // Добавляем элементы на форму
          Controls.AddRange(new Control[] 
