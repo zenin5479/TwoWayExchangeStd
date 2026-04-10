accf2b27e2a69a6f36ad8134512f2834a79f5720
zenin5479 <zenin5479@gmail.com>
2026-04-10 15:11:14 +0300

    Update 13.9 Обновлен метод. Проведен рефакторинг

--- ConsoleAppOne/DataForm.Designer.cs
+++ ConsoleAppOne/DataForm.Designer.cs
@@ -85,7 +85,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(346, 212);
+         ClientSize = new System.Drawing.Size(225, 212);
          Controls.Add(closeButton);
          Controls.Add(commentTextBox);
          Controls.Add(commentLabel);
