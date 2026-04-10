7861bb340e8c127b1c7fdf0b43fb8d26504693e7
zenin5479 <zenin5479@gmail.com>
2026-04-10 14:31:51 +0300

    Update 12.4 Обновлен метод. Проведен рефакторинг

---
+++ ConsoleAppOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppOne
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
