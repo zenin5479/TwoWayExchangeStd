a981038ce0d97163ce500ccb2e7feaa1d62d890c
zenin5479 <zenin5479@gmail.com>
2026-04-10 13:07:09 +0300

    Update 11.5 Обновлен метод. Проведен рефакторинг

---
+++ ConsoleAppTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTwo
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
