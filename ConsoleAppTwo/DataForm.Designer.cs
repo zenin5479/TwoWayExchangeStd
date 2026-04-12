2bc7c1a0d33084dee2ca5b77600b8b442d1fa408
zenin5479 <zenin5479@gmail.com>
2026-04-12 09:28:22 +0300

    Update 16.8 Обновлен метод. Проведен рефакторинг

--- ConsoleAppTwo/DataForm.Designer.cs
+++ ConsoleAppTwo/DataForm.Designer.cs
@@ -28,12 +28,80 @@
       /// </summary>
       private void InitializeComponent()
       {
-         components = new System.ComponentModel.Container();
+         greetingLabel = new System.Windows.Forms.Label();
+         closeButton = new System.Windows.Forms.Button();
+         commentTextBox = new System.Windows.Forms.TextBox();
+         commentLabel = new System.Windows.Forms.Label();
+         agreeCheckBox = new System.Windows.Forms.CheckBox();
+         SuspendLayout();
+         // 
+         // greetingLabel
+         // 
+         greetingLabel.AutoSize = true;
+         greetingLabel.Location = new System.Drawing.Point(33, 29);
+         greetingLabel.Name = "greetingLabel";
+         greetingLabel.Size = new System.Drawing.Size(115, 15);
+         greetingLabel.TabIndex = 10;
+         greetingLabel.Text = "Данные из консоли";
+         // 
+         // closeButton
+         // 
+         closeButton.Location = new System.Drawing.Point(33, 166);
+         closeButton.Name = "closeButton";
+         closeButton.Size = new System.Drawing.Size(174, 23);
+         closeButton.TabIndex = 9;
+         closeButton.Text = "Закрыть и передать данные";
+         closeButton.UseVisualStyleBackColor = true;
+         // 
+         // commentTextBox
+         // 
+         commentTextBox.Location = new System.Drawing.Point(33, 87);
+         commentTextBox.Multiline = true;
+         commentTextBox.Name = "commentTextBox";
+         commentTextBox.Size = new System.Drawing.Size(315, 73);
+         commentTextBox.TabIndex = 8;
+         // 
+         // commentLabel
+         // 
+         commentLabel.AutoSize = true;
+         commentLabel.Location = new System.Drawing.Point(33, 69);
+         commentLabel.Name = "commentLabel";
+         commentLabel.Size = new System.Drawing.Size(87, 15);
+         commentLabel.TabIndex = 7;
+         commentLabel.Text = "Комментарий:";
+         // 
+         // agreeCheckBox
+         // 
+         agreeCheckBox.AutoSize = true;
+         agreeCheckBox.Location = new System.Drawing.Point(33, 47);
+         agreeCheckBox.Name = "agreeCheckBox";
+         agreeCheckBox.Size = new System.Drawing.Size(159, 19);
+         agreeCheckBox.TabIndex = 6;
+         agreeCheckBox.Text = "Я согласен с условиями";
+         agreeCheckBox.UseVisualStyleBackColor = true;
+         // 
+         // DataForm
+         // 
+         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(800, 450);
+         Controls.Add(greetingLabel);
+         Controls.Add(closeButton);
+         Controls.Add(commentTextBox);
+         Controls.Add(commentLabel);
+         Controls.Add(agreeCheckBox);
+         Name = "DataForm";
          Text = "Form1";
+         ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.Label greetingLabel;
+      private System.Windows.Forms.Button closeButton;
+      private System.Windows.Forms.TextBox commentTextBox;
+      private System.Windows.Forms.Label commentLabel;
+      private System.Windows.Forms.CheckBox agreeCheckBox;
    }
 }
