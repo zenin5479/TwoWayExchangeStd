08e6fc332e0adbd96f07600b62927129313e31b4
zenin5479 <zenin5479@gmail.com>
2026-04-13 09:41:31 +0300

    Update 19.9 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppThree/MainForm.Designer.cs
+++ ConsoleAppThree/MainForm.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(584, 261);
+         ClientSize = new System.Drawing.Size(384, 261);
          Name = "MainForm";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "MainForm";
