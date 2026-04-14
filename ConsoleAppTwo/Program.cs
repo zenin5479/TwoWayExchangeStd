981bf128e19f14b6cb542c2c9b42294e59aafdb8
zenin5479 <zenin5479@gmail.com>
2026-04-14 18:05:40 +0300

    Update 23.7 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppTwo/Program.cs
+++ ConsoleAppTwo/Program.cs
@@ -31,7 +31,8 @@ namespace ConsoleAppTwo
          Console.WriteLine(@"Пользователь согласился: {0}", userChoice);
          Console.WriteLine(@"Комментарий: {0}", userComment);
 
-         Console.WriteLine("\nНажмите любую клавишу для выхода...");
+         Console.WriteLine(@"
+Нажмите любую клавишу для выхода...");
          Console.ReadKey();
       }
    }
