ec9b8a4a986055512fdade885e89ebebde026153
zenin5479 <zenin5479@gmail.com>
2026-04-14 18:01:18 +0300

    Update 22.9 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppThree/MainForm.cs
+++ ConsoleAppThree/MainForm.cs
@@ -12,7 +12,7 @@ namespace ConsoleAppThree
 
       private void helloButton_Click(object sender, EventArgs e)
       {
-         messageLabel.Text = string.Format("Привет! Время: {0:HH:mm:ss}", DateTime.Now);
+         messageLabel.Text = string.Format(@"Привет! Время: {0:HH:mm:ss}", DateTime.Now);
       }
    }
 }
