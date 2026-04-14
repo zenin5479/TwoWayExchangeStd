e0464ab0e623f6b358a56c2994ad4bc0e31a744e
zenin5479 <zenin5479@gmail.com>
2026-04-14 17:55:53 +0300

    Update 22.5 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppThree/Program.cs
+++ ConsoleAppThree/Program.cs
@@ -23,7 +23,7 @@ namespace ConsoleAppThree
 
          // Этот код выполнится только после закрытия формы
          Console.WriteLine(@"Форма закрыта. Консоль снова активна!");
-         Console.WriteLine("Нажмите любую клавишу для выхода...");
+         Console.WriteLine(@"Нажмите любую клавишу для выхода...");
          Console.ReadKey();
       }
    }
