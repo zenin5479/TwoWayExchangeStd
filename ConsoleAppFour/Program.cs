4668056086330d588e055fcb7472ff7aaab0d3d9
zenin5479 <zenin5479@gmail.com>
2026-04-15 21:18:39 +0300

    Update 28.0 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppFour/Program.cs
+++ ConsoleAppFour/Program.cs
@@ -10,14 +10,15 @@ namespace ConsoleAppFour
       [STAThread]
       static void Main()
       {
-         Console.WriteLine(@"Введите сообщение для формы:");
-         string consoleData = Console.ReadLine();
+         Console.WriteLine("Введите текст для отображения в форме:");
+         string userInput = Console.ReadLine();
 
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
+         Application.Run(new MainForm(userInput));
 
-         // Передаём данные из консоли в форму через конструктор
-         Application.Run(new FormConstructor(consoleData));
+         Console.WriteLine("Работа завершена. Нажмите любую клавишу...");
+         Console.ReadKey();
       }
    }
 }
