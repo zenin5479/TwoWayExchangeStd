7bb038422e7fe737821abb0b5e7932f1c58d942d
zenin5479 <zenin5479@gmail.com>
2026-04-15 20:22:18 +0300

    Update 24.9 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppFour/FormConstructor.Designer.cs
+++ ConsoleAppFour/FormConstructor.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(800, 450);
+         ClientSize = new System.Drawing.Size(384, 261);
          Name = "FormConstructor";
          Text = "Обмен данными";
          ResumeLayout(false);
