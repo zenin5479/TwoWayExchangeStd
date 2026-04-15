c0c2dceede9b1e8dbb1b54ace4caf82073bf9d17
zenin5479 <zenin5479@gmail.com>
2026-04-15 10:58:18 +0300

    Update 24.0 Обновлен метод. Проведен рефакторинг. Рабочий код

---
+++ ConsoleAppFour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppFour
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
