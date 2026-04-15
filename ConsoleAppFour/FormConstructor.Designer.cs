eba7daa12a465054585e4bf67d945615a9c3aaca
zenin5479 <zenin5479@gmail.com>
2026-04-15 20:30:51 +0300

    Update 26.2 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppFour/FormConstructor.Designer.cs
+++ ConsoleAppFour/FormConstructor.Designer.cs
@@ -63,7 +63,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(201, 261);
+         ClientSize = new System.Drawing.Size(257, 190);
          Controls.Add(returnButton);
          Controls.Add(inputBox);
          Controls.Add(messageLabel);
