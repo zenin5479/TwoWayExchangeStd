1daa70a4d39e4eca279bd16a66e40321e97ef12d
zenin5479 <zenin5479@gmail.com>
2026-04-16 10:49:02 +0300

    Update 29.0 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppFive/Program.cs
+++ ConsoleAppFive/Program.cs
@@ -4,6 +4,9 @@ namespace ConsoleAppFive
 {
    internal class Program
    {
+      // Статическое поле для обмена данными
+      public static string ReturnedData { get; set; } = "Нет данных";
+
       static void Main()
       {
          Console.WriteLine("Hello World!");
