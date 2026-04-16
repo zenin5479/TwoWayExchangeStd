cd28a7bb2526d161004e7de4261fc9b6ecba7818
zenin5479 <zenin5479@gmail.com>
2026-04-16 10:32:18 +0300

    Update 28.3 Обновлен метод. Проведен рефакторинг. Рабочий код

---
+++ ConsoleAppFive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppFive
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
