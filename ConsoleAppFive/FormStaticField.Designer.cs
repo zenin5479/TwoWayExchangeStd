fd3138d428edc954a0fc5a97abaa4ee56f0a72ae
zenin5479 <zenin5479@gmail.com>
2026-04-17 14:09:20 +0300

    Update 30.3 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppFive/FormStaticField.Designer.cs
+++ ConsoleAppFive/FormStaticField.Designer.cs
@@ -55,6 +55,7 @@
          // textBox1
          // 
          textBox1.Location = new System.Drawing.Point(42, 83);
+         textBox1.Multiline = true;
          textBox1.Name = "textBox1";
          textBox1.Size = new System.Drawing.Size(100, 23);
          textBox1.TabIndex = 3;
