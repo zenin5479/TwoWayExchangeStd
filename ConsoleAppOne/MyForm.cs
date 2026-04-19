da8dc36d1eb924d876a3ba685d2cc3ae452ad83e
zenin5479 <zenin5479@gmail.com>
2026-04-19 11:32:48 +0300

    Update 35.5 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppOne/MyForm.cs
+++ ConsoleAppOne/MyForm.cs
@@ -18,6 +18,7 @@ namespace ConsoleAppOne
       private void helloButton_Click(object sender, EventArgs e)
       {
          messageLabel.Text = string.Format(@"Привет! Время: {0:HH:mm:ss}", DateTime.Now);
+         Console.WriteLine(@"Консоль снова активна!");
       }
    }
 }
