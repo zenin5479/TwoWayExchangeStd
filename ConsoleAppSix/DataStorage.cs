84bb9f621387b38f489d71e77b138113b3417e4b
zenin5479 <zenin5479@gmail.com>
2026-04-22 10:25:09 +0300

    Update 45.0 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSix/DataStorage.cs
+++ ConsoleAppSix/DataStorage.cs
@@ -1,7 +1,28 @@
-﻿namespace ConsoleAppSix
-{
-   internal class DataStorage
-   {
+﻿using System.Collections.Generic;
 
+namespace ConsoleAppSix
+{
+   public static class DataStorage
+   {
+      // Простые типы данных
+      public static string UserName { get; set; }
+      public static int UserAge { get; set; }
+
+      // Сложные типы
+      public static List<string> Messages { get; set; } = new List<string>();
+      public static Dictionary<string, object> Settings { get; set; } =
+         new Dictionary<string, object>();
+
+      // Временные данные
+      public static bool IsFormActive { get; set; }
+
+      // Метод для сброса всех данных
+      public static void ClearAll()
+      {
+         UserName = null;
+         UserAge = 0;
+         Messages.Clear();
+         Settings.Clear();
+      }
    }
 }
