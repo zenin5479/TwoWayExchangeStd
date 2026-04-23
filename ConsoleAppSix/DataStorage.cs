5188052dde964e37ab3ecfea2210bf83dfd2748b
zenin5479 <zenin5479@gmail.com>
2026-04-23 21:32:36 +0300

    Update 44.7 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSix/DataStorage.cs
+++ ConsoleAppSix/DataStorage.cs
@@ -4,13 +4,19 @@ namespace ConsoleAppSix
 {
    public static class DataStorage
    {
+      private static Dictionary<string, object> _settings = new Dictionary<string, object>();
+
       // Простые типы данных
       public static string UserName { get; set; }
       public static int UserAge { get; set; }
 
       // Сложные типы
       public static List<string> Messages { get; set; } = new List<string>();
-      public static Dictionary<string, object> Settings { get; set; } = new Dictionary<string, object>();
+
+      public static Dictionary<string, object> Settings
+      {
+         get { return _settings; }
+      }
 
       // Временные данные
       public static bool IsFormActive { get; set; }
