82dd1dd142ed7405b4399436a59bc7ca06985414
zenin5479 <zenin5479@gmail.com>
2026-04-25 21:33:58 +0300

    Update 48.3 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSeven/DataEditorForm.Designer.cs
+++ ConsoleAppSeven/DataEditorForm.Designer.cs
@@ -32,6 +32,7 @@
          nameTextBox = new System.Windows.Forms.TextBox();
          ageTextBox = new System.Windows.Forms.TextBox();
          emailTextBox = new System.Windows.Forms.TextBox();
+         button1 = new System.Windows.Forms.Button();
          SuspendLayout();
          // 
          // dataListBox
@@ -64,11 +65,21 @@
          emailTextBox.Size = new System.Drawing.Size(150, 23);
          emailTextBox.TabIndex = 3;
          // 
+         // button1
+         // 
+         button1.Location = new System.Drawing.Point(12, 217);
+         button1.Name = "button1";
+         button1.Size = new System.Drawing.Size(75, 23);
+         button1.TabIndex = 4;
+         button1.Text = "button1";
+         button1.UseVisualStyleBackColor = true;
+         // 
          // DataEditorForm
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(484, 361);
+         Controls.Add(button1);
          Controls.Add(emailTextBox);
          Controls.Add(ageTextBox);
          Controls.Add(nameTextBox);
@@ -86,5 +97,6 @@
       private System.Windows.Forms.TextBox nameTextBox;
       private System.Windows.Forms.TextBox ageTextBox;
       private System.Windows.Forms.TextBox emailTextBox;
+      private System.Windows.Forms.Button button1;
    }
 }
