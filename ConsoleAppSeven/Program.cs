17475e5af19aa220b83b0b4d95c6fe44291e34c8
zenin5479 <zenin5479@gmail.com>
2026-04-25 21:11:58 +0300

    Update 45.7 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSeven/Program.cs
+++ ConsoleAppSeven/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleAppSeven
 {
    internal class Program
    {
-      static void Main(string[] args)
+      static void Main()
       {
          Console.WriteLine("Hello World!");
       }
