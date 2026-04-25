2d902a8abfa060400e38f7a8c75e1988fd5321bc
zenin5479 <zenin5479@gmail.com>
2026-04-25 21:21:24 +0300

    Update 46.7 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSeven/DataEditorForm.Designer.cs
+++ ConsoleAppSeven/DataEditorForm.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(384, 394);
+         ClientSize = new System.Drawing.Size(484, 394);
          Name = "DataEditorForm";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Редактор данных";
