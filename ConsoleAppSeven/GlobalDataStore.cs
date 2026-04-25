efc3131260b5eb59936346c7cce3d3d63467c3d0
zenin5479 <zenin5479@gmail.com>
2026-04-25 21:15:05 +0300

    Update 45.9 Обновлен метод. Проведен рефакторинг. Рабочий код

---
+++ ConsoleAppSeven/GlobalDataStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppSeven
+{
+   public class GlobalDataStore
+   {
+
+      public static List<Person> People { get; } = new List<Person>();
+
+   }
+
+   public class Person
+   {
+      public string Name { get; set; }
+      public int Age { get; set; }
+      public string Email { get; set; }
+
+      public override string ToString() => $"{Name}, {Age} лет, {Email}";
+   }
+}
