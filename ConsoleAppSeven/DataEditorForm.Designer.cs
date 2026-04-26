8af1d467d7a46f6b4973f2296bbeca3514a3ea94
zenin5479 <zenin5479@gmail.com>
2026-04-26 21:45:17 +0300

    Update 53.9 Обновлен метод. Проведен рефакторинг. Рабочий код

--- ConsoleAppSeven/DataEditorForm.Designer.cs
+++ ConsoleAppSeven/DataEditorForm.Designer.cs
@@ -38,6 +38,7 @@
          selectedIndexLabel = new System.Windows.Forms.Label();
          LabelName = new System.Windows.Forms.Label();
          LabelAge = new System.Windows.Forms.Label();
+         label1 = new System.Windows.Forms.Label();
          SuspendLayout();
          // 
          // dataListBox
@@ -128,11 +129,21 @@
          LabelAge.TabIndex = 9;
          LabelAge.Text = "Возраст:";
          // 
+         // label1
+         // 
+         label1.AutoSize = true;
+         label1.Location = new System.Drawing.Point(263, 284);
+         label1.Name = "label1";
+         label1.Size = new System.Drawing.Size(38, 15);
+         label1.TabIndex = 10;
+         label1.Text = "label1";
+         // 
          // DataEditorForm
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(484, 361);
+         Controls.Add(label1);
          Controls.Add(LabelAge);
          Controls.Add(LabelName);
          Controls.Add(selectedIndexLabel);
@@ -162,5 +173,6 @@
       private System.Windows.Forms.Label selectedIndexLabel;
       private System.Windows.Forms.Label LabelName;
       private System.Windows.Forms.Label LabelAge;
+      private System.Windows.Forms.Label label1;
    }
 }
